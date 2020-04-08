2e1d7be066b3fae64583aa145d2144c39a0bd825
Hakyves <hakyves@gmail.com>
2020-04-08 14:41:55 -0400

    second commit

--- src/main/java/com/hak/controller/YvesController.java
+++ src/main/java/com/hak/controller/YvesController.java
@@ -1,5 +1,13 @@
 package com.hak.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class YvesController {
+	@GetMapping("/")
+	public String getMessage() {
+		return "welcome to spgBootGitJenkinsIntegration";
+	}
 
 }
